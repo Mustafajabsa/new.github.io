4a22a7e4b6dc39682fe4315f5f915e3ba5b01f01
Mustafajabsa <110934875+Mustafajabsa@users.noreply.github.com>
2023-04-21 06:46:13 +0530

    Add files via upload

--- form-handler.php
+++ form-handler.php
@@ -3,7 +3,7 @@ $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
-$email_from = 'info@mustafajabsa.github.io/new.github.io/';
+$email_from = 'jabsa2020@gmail.com';
 $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n" .
     "User Email: $visitor_email.\n" .
